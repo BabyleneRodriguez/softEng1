4f0a29f958892b13a80305eea67b5a82e0de8c74
Babylene Rodriguez <142875371+BabyleneRodriguez@users.noreply.github.com>
2023-09-01 12:43:04 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello{
   public static void main(String[] args){
-    System.out.println("Hello, Software Engineering Course!");
-    System.out.println("My name is RODRIGUEZ, Babylene!");
+   System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }
